6739f6804475e3672daf78eea869984745759a5b
doorgod <425308978@qq.com>
2019-09-09 16:12:53 +0800

    test

--- src/main.js
+++ src/main.js
@@ -4,18 +4,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-import router from './router';
-import NProgress from 'nprogress'; // Progress 进度条
-import 'nprogress/nprogress.css';// Progress 进度条 样式
-import store from './store';
-import * as filters from './filters'; // 全局vue filter
-import * as directives from './directive'; // 全局vue directives
-import components from './components/';
-import Polyfill from 'utils/legacy/index';
+import router from './router'
+import NProgress from 'nprogress' // Progress 进度条
+import 'nprogress/nprogress.css'// Progress 进度条 样式
+import store from './store'
+import * as filters from './filters' // 全局vue filter
+import * as directives from './directive' // 全局vue directives
+import components from './components/'
+import Polyfill from 'utils/legacy/index'
 import './plugins/element.js'
 
 //Polyfill
-Polyfill.walk();
+Polyfill.walk()
 
 // 注册全局 filter
 Object.keys(filters).forEach(key => {
@@ -25,7 +25,7 @@ Object.keys(filters).forEach(key => {
 // 注册全局指令
 Object.keys(directives).forEach(key => {
 	Vue.directive(key, directives[key])
-});
+})
 
 // 全局组件注册
 Object.keys(components).forEach((key) => {
@@ -35,17 +35,17 @@ Object.keys(components).forEach((key) => {
 /**
  * 路由过场动画--start
  */
-Vue.cancel = [];
+Vue.cancel = []
 // eslint-disable-next-line no-unused-vars
 router.beforeEach((to, from, next) => {
 	NProgress.start(); // 开启Progress
 	while (Vue.cancel.length > 0) {
-		Vue.cancel.pop()('cancel');
+		Vue.cancel.pop()('cancel')
 	}
 	try {
-		NProgress.done();
+		NProgress.done()
 	} catch (err) {
-		NProgress.done();
+		NProgress.done()
 	}
 });
 
@@ -53,13 +53,13 @@ router.beforeEach((to, from, next) => {
  * 路由过场动画--end
  */
 router.afterEach(() => {
-	NProgress.done(); // 结束Progress
+	NProgress.done() // 结束Progress
 });
 
 new Vue({
 	router,
 	store,
 	render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
 
 
