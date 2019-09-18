a6e3caaf8b49d51faa57544def82d4b0b156797e
doorgod <425308978@qq.com>
2019-09-18 20:47:35 +0800

    子账号列表

--- src/router/homeChildren.js
+++ src/router/homeChildren.js
@@ -9,6 +9,12 @@ const children = [
 		path: '/userInfo',
 		name: 'userInfo',
 		component: () => import('@/views/userinfo/UserInfo.vue')
+	},
+
+	{
+		path: '/accountList',
+		name: 'accountList',
+		component: () => import('@/views/account/AccountList.vue')
 	}
 ]
 
---
+++ src/views/account/AccountList.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="account-list">
+        <div>
+            <el-row :gutter="24">
+                <el-col :span="6">
+                    <el-card>
+                        <p class="card-name">实时算力</p>
+                        <p class="card-value">
+                            34.1
+                            <span>Th/s</span>
+                        </p>
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card>
+                        <p class="card-name">实时算力</p>
+                        <p class="card-value">
+                            34.1
+                            <span>Th/s</span>
+                        </p>
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card>
+                        <p class="card-name">实时算力</p>
+                        <p class="card-value">
+                            34.1
+                            <span>Th/s</span>
+                        </p>
+                    </el-card>
+                </el-col>
+                <el-col :span="6">
+                    <el-card>
+                        <p class="card-name">实时算力</p>
+                        <p class="card-value">
+                            34.1
+                            <span>Th/s</span>
+                        </p>
+                    </el-card>
+                </el-col>
+            </el-row>
+        </div>
+
+        <div class="list-warp">
+            <div class="list-wrap-top">
+                <el-row :gutter="24" align="middle">
+                    <el-col :span="12">
+                        <p class="title">子账号列表</p>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-input
+                            placeholder="请输入内容"
+                            suffix-icon="el-icon-search">
+                        </el-input>
+                    </el-col>
+                    <el-col :span="3">
+                        <el-button>默认按钮</el-button>
+                    </el-col>
+                    <el-col :span="3">
+                        <el-button type="primary">主要按钮</el-button>
+                    </el-col>
+                </el-row>
+
+            </div>
+
+            <div class="table-warp">
+                <el-table stripe>
+                    <el-table-column
+                        prop="date"
+                        label="子账号名称">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="实施算力">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="一小时算力">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="活跃矿机">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="失效矿机">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="操作">
+                    </el-table-column>
+                </el-table>
+                <p class="totle">共400条记录 第1/80页</p>
+            </div>
+
+            <div class="page-warp">
+                <el-pagination
+                    background
+                    :page-sizes="[10, 20, 30, 40]"
+                    :page-size="100"
+                    layout="prev, pager, next, sizes, jumper"
+                    :total="400">
+                </el-pagination>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'AccountList'
+	}
+</script>
+
