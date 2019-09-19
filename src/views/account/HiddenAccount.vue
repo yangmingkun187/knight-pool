c010fe1446e80aea895c5821c3cada8f15a0f722
doorgod <425308978@qq.com>
2019-09-20 00:03:18 +0800

    子账号隐藏列表、观察者列表

---
+++ src/views/account/HiddenAccount.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="second-level-list">
+
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/accountList' }">子账号列表</el-breadcrumb-item>
+            <el-breadcrumb-item>已隐藏子账号列表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <div class="list-warp">
+            <div class="list-wrap-top">
+                <el-row :gutter="24" align="middle">
+                    <el-col :span="12">
+                        <p class="title">已隐藏账号列表</p>
+                    </el-col>
+                </el-row>
+            </div>
+
+            <div class="table-warp">
+                <el-table stripe>
+                    <el-table-column
+                        prop="date"
+                        label="子账号名称">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="实时算力">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="一小时算力">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="活跃矿机">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="失效矿机">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="操作">
+                    </el-table-column>
+                </el-table>
+                <p class="tips">共400条记录 第1/80页</p>
+            </div>
+
+            <div style="text-align: right">
+                <el-pagination
+                    background
+                    :page-sizes="[10, 20, 30, 40]"
+                    :page-size="100"
+                    layout="prev, pager, next, sizes, jumper"
+                    :total="400">
+                </el-pagination>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
---
+++ src/views/account/ObserverList.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="second-level-list">
+
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/accountList' }">子账号列表</el-breadcrumb-item>
+            <el-breadcrumb-item>设置观察者链接</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <div class="list-warp">
+            <div class="list-wrap-top">
+                <el-row :gutter="24" align="middle">
+                    <el-col :span="20">
+                        <p class="title">观察者列表</p>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-button type="primary">创建新的观察者</el-button>
+                    </el-col>
+                </el-row>
+            </div>
+
+            <div class="table-warp">
+                <el-table stripe>
+                    <el-table-column
+                        prop="date"
+                        label="观察者名称">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="备注说明">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="链接地址">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="状态">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="操作">
+                    </el-table-column>
+                </el-table>
+                <p class="tips">共400条记录 第1/80页</p>
+            </div>
+
+            <div style="text-align: right">
+                <el-pagination
+                    background
+                    :page-sizes="[10, 20, 30, 40]"
+                    :page-size="100"
+                    layout="prev, pager, next, sizes, jumper"
+                    :total="400">
+                </el-pagination>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
